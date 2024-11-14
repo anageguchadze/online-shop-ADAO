6f9e07a2811c1e738427cd0aaddc10d6839088ff
Ana Geguchadze <anageguchadze22@gmail.com>
2024-11-14 12:54:57 +0100

    Initial Project Setup

---
+++ adao_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class AdaoAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'adao_app'
---
+++ adao_app/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+class Product(models.Model):
+    CATEGORY_CHOICES = [
+        ('candles', 'Candles'),
+        ('clay', 'Clay')
+    ]
+
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    category = models.CharField(choices=CATEGORY_CHOICES)
+    image = models.ImageField(upload_to='product_images/', blank=True, null=True)
+
+    def __str__(self):
+        return self.name
