d510dae80351619b7e5786ec8048be268776ebdf
Ana Geguchadze <anageguchadze22@gmail.com>
2024-11-14 14:23:36 +0100

    Registered models in admin panel for product management

--- adao_app/admin.py
+++ adao_app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Product
 
-# Register your models here.
+# Register your models here. admin - admin123
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'image')  # სურათის ჩვენება
+
+admin.site.register(Product, ProductAdmin)
